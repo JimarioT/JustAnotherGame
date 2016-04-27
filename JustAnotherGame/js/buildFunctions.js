ed3a970515c5631cbe0727f7eb5c577e2a8fc876
JimarioT <mtoutountzis@yahoo.gr>
2016-04-27 23:38:53 +0300

    Added files via upload

--- JustAnotherGame/js/buildFunctions.js
+++ JustAnotherGame/js/buildFunctions.js
@@ -104,40 +104,43 @@ function addBuildingToList(building) {
 	var totalBuildings = campaignInfo.baseInfo.buildings[building].buildingInfo.builtStatus;
 	
 	var item = '<div data-building="' + buildingName.replace(" ", "") + totalBuildings.length + '" data-workers="0" data-maxWorkers="' + maxWorkers + '" data-level="1">' +
-					'<a>' + buildingName + '<span class="' + building + 'Lvl"> LvL 1</span></a>' +
+					'<a>' + buildingName + '<span class="' + building + 'Lvl"> LvL 1</span></a>' +				
 					'<i class="fa fa-minus foodMinus" onclick="removeWorkforce(\'' + jobName + '\', this)"></i>' +
 					'<span id="" class="'+ jobName +'Workforce"> 0 </span>' +
-					'<i class="fa fa-plus foodPlus" onclick="calculateWorkforce(\'' + jobName + '\', this)"></i>' +
+					'<i class="fa fa-plus foodPlus" onclick="calculateWorkforce(\'' + jobName + '\', this)"></i>' +	
 				'</div>';
 
 	var itemPreparing = '<div class="progress">' + 
-							'<div class="' + buildingName.replace(" ", "") + totalBuildings.length + '-progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">' + 
-								'60%' + 
+							'<div class="' + buildingName.replace(" ", "") + totalBuildings.length + '-progress-bar progress-bar" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100">' + 								
 							'</div>' + 
 						'</div>';
 
 	var prod = campaignInfo.baseInfo.buildings[building].buildingInfo.levelBonus[0].productionBonus;
-	var index = {'level': 1, 'productionBonus': prod, 'currentlyWorking': 0, 'maxAllowedWorkers': maxWorkers};	
-	//var timeRequired = campaignInfo.baseInfo.buildings[building].buildingInfo.levelBonus[0].timeRquired;
-	//var timeIntervalSteps = timeRequired / 100;
-	//var timePassed = 0;
-	
-	//var buildInterval = setInterval(function() {
-		//if(timeIntervals < 100) {
-			//$('.' + buildingName.replace(" ", "") + totalBuildings.length + '-progress-bar').css('width', timePassed + '%');
-			//$('.' + buildingName.replace(" ", "") + totalBuildings.length + '-progress-bar').attr('aria-valuenow', timePassed);
-			//$('.' + buildingName.replace(" ", "") + totalBuildings.length + '-progress-bar').text(timePassed);
-			//timePassed += timeIntervalSteps;
-			//console.log($('.' + buildingName.replace(" ", "") + totalBuildings.length + '-progress-bar').css('width'));
-		//}
-		//else {
-			//clearInterval(buildInterval);
-		//}
-	//}, 1000);
+	var index = {'level': 1, 'productionBonus': prod, 'currentlyWorking': 0, 'maxAllowedWorkers': maxWorkers};
 
-	//totalBuildings.push(index);
-	$('.hovelList').append(item);		
+	var timeRequired = campaignInfo.baseInfo.buildings[building].buildingInfo.levelBonus[0].timeRquired;
+	var percentageSteps = 100 / timeRequired;
 	$('.hovelList').append(itemPreparing);
+	var timePassed = 0;
+	var percentageTotal = 0;
+	
+	var buildInterval = setInterval(function() {
+		if(timePassed < timeRequired) {
+			timePassed++;
+			percentageTotal += percentageSteps;			
+			$('.' + buildingName.replace(" ", "") + totalBuildings.length + '-progress-bar').css('width', parseInt(percentageTotal) + '%');
+			$('.' + buildingName.replace(" ", "") + totalBuildings.length + '-progress-bar').attr('aria-valuenow', parseInt(percentageTotal));
+			$('.' + buildingName.replace(" ", "") + totalBuildings.length + '-progress-bar').text(parseInt(percentageTotal) + '%');			
+		}
+		else {
+			clearInterval(buildInterval);
+			$('.hovelList').empty();
+			totalBuildings.push(index);
+			$('.hovelList').append(item);		
+		}
+	}, 1000);
+
+	
 	$('.playersGold').text(resources.gold + ' / ' + resources.goldStorage);
 	$('.playersWood').text(resources.wood + ' / ' + resources.woodStorage);
 	$('.playersStone').text(resources.stone + ' / ' + resources.stoneStorage);
